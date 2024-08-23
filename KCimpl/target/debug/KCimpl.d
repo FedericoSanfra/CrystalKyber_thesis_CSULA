b2df7a750c5b39e443c6670794a085be41de557e
FedericoSanfra <s308344@studenti.polito.it>
2024-08-22 17:53:18 -0700

    creating project

---
+++ KCimpl/target/debug/KCimpl.d
@@ -0,0 +1,1 @@
+C:\Users\feder\RustroverProjects\KCimpl\thesis\KCimpl\target\debug\KCimpl.exe: C:\Users\feder\RustroverProjects\KCimpl\thesis\KCimpl\src\main.rs
---
+++ KCimpl/target/debug/deps/KCimpl-c3ae8ff726f145ad.d
@@ -0,0 +1,5 @@
+C:\Users\feder\RustroverProjects\KCimpl\thesis\KCimpl\target\debug\deps\KCimpl-c3ae8ff726f145ad.exe: src\main.rs
+
+C:\Users\feder\RustroverProjects\KCimpl\thesis\KCimpl\target\debug\deps\KCimpl-c3ae8ff726f145ad.d: src\main.rs
+
+src\main.rs:
